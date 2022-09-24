93ae39701714fb9d5e483f4ba1eca841a1458350
liubing <943485115@qq.com>
2022-09-24 16:45:14 +0800

    echart图组件数据监听

--- src/components/common/Tchartpie.vue
+++ src/components/common/Tchartpie.vue
@@ -74,7 +74,8 @@ export default {
     options: {
       deep: true,
       handler (newV) {
-        this.setOptions(newV)
+        const options = this.getOptions(newV)
+        this.setOptions(options)
       }
     }
   },
